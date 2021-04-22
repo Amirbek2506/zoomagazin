31b912809997dacb9a602ac44487f29009c2bd2f
Amirbek2506 <alikmirzoev1999@gmail.com>
2021-04-22 10:39:05 +0500

    change token term

--- Controllers/AuthenticateController.cs
+++ Controllers/AuthenticateController.cs
@@ -61,7 +61,7 @@
                     var token = new JwtSecurityToken(
                         issuer: _configuration["JWT:ValidIssuer"],
                         audience: _configuration["JWT:ValidAudience"],
-                        expires: DateTime.Now.AddHours(3),
+                        expires: DateTime.Now.AddMonths(1),
                         claims: authClaims,
                         signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
                         );
