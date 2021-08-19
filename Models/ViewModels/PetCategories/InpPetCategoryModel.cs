bb418fab247cf2957377c89162b4dc4bb6dbd1ca
Nodira Kosimova <nkosimova06@gmail.com>
2021-08-19 13:25:29 +0500

    uncomment PetCategories service

--- Models/ViewModels/PetCategories/InpPetCategoryModel.cs
+++ Models/ViewModels/PetCategories/InpPetCategoryModel.cs
@@ -8,7 +8,7 @@ namespace ZooMag.Models.ViewModels.PetCategories
 {
     public class InpPetCategoryModel
     {
-        public int ParentId { get; set; }
+        public int? ParentId { get; set; }
         public string Title { get; set; }
         public IFormFile Image { get; set; }
     }
